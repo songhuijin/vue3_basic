04ad76ffa75d92e0069495663d5b46f75316ea99
宋慧锦 <songhuijin_xm.cicdi@chinaccs.cn>
2021-01-14 17:28:43 +0800

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <p>{{count}}</p>
+  <!-- <p>{{count}}</p>
   <p>{{double}}</p>
-  <button @click="add">+1</button>
+  <button @click="add">+1</button> -->
+  <p>{{data.count}}</p>{{count}}
+  <p>{{data.double}}</p>
+  <button @click="data.addFun">+1</button>
 </template>
 
 <script lang="ts">
-import { ref ,computed} from 'vue'
+// import { ref ,computed} from 'vue'
+import {reactive,computed,toRefs} from 'vue'
+// interface Idata {
+//   count : number;
+//   addFun : ()=>{};
+//   double : number; 
+// }
 export default({
   name: 'App',
   setup(){
-    const count = ref(1)
-    const double = computed (()=>{
-      return count.value * 2
+    // const count = ref(1)
+    // const double = computed (()=>{
+    //   return count.value * 2
+    // })
+    // const add =()=>{
+    //   count.value ++ 
+    // }
+    const data= reactive({
+      count:0,
+      addFun:()=>{
+         data.count++
+      },
+      double:computed(()=>{
+         data.count *2
+      })
     })
-    const add =()=>{
-      count.value ++ 
-    }
-
+    const refsData = toRefs(data)
     return {
-      count,
-      add,
-      double
+      data,
+      ...refsData
+      // count,
+      // add,
+      // double
     }
   },
   
